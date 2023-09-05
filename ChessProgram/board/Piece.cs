c04fdfa70b5646f8a312968f1b794c17685638c0
Júlia Patricia Wolschick <juliapwols@pm.me>
2023-09-05 15:30:30 -0300

    Created more relevant classes

---
+++ ChessProgram/board/Piece.cs
@@ -0,0 +1,17 @@
+﻿namespace board {
+    class Piece {
+
+        public Position position { get; set; }
+        public Color color { get; protected set; }
+        public int moveCount { get; protected set; }
+
+        public Board board { get; protected set; }
+
+        public Piece(Position position, Color color, Board board) {
+            this.position = position;
+            this.color = color;
+            this.board = board;
+            this.moveCount = 0;
+        }
+    }
+}
