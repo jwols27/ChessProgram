62b918a7cbbce82fb8ef6eac39987d844efc87c2
João Pedro Wolschick <joaopedrowols@gmail.com>
2023-09-05 16:48:45 -0300

    Embellished chess board

--- ChessProgram/GameView.cs
+++ ChessProgram/GameView.cs
@@ -3,15 +3,37 @@ using board;
 
 namespace ChessProgram {
     class GameView {
-        public static void viewBoard(Board board) {
+        public static void renderBoard(Board board) {
             for(int i = 0; i < board.rows; i++) {
+                Console.Write(8 - i + " ");
                 for (int j = 0; j < board.columns; j++) {
-                    if (board.isPositionEmpty(new Position(i, j)))
+                    Position pos = new Position(i, j);
+                    if (board.isPositionEmpty(pos))
                         Console.Write("-");
-                    Console.Write(board.piece(new Position(i, j)) + " ");
+                    else
+                        renderPiece(board.piece(pos));
+                    Console.Write(" ");
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void renderPiece(Piece p) {
+            ConsoleColor aux = Console.ForegroundColor;
+            switch (p.color) {
+                case Color.Black:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case Color.Red:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                default:
+                    break;
+            }
+            Console.Write(p);
+            Console.ForegroundColor = aux;
         }
     }
+
 }
---
+++ ChessProgram/chess/ChessPosition.cs
@@ -0,0 +1,22 @@
+﻿using board;
+
+namespace chess {
+    class ChessPosition {
+
+        public int row { get; set; }
+        public char column { get; set; }
+
+        public ChessPosition(int row, char column) {
+            this.row = row;
+            this.column = column;
+        }
+
+        public Position toPosition() {
+            return new Position(8 - row, column - 'a');
+        }
+
+        public override string ToString() {
+            return "" + column + row;
+        }
+    }
+}
