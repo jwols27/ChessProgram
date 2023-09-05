cdfa20c7ca440c8ce94a190e3a87455a213284ba
Júlia Patricia Wolschick <juliapwols@pm.me>
2023-09-05 15:38:04 -0300

    Created Game Screen

---
+++ ChessProgram/GameView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using board;
+
+namespace ChessProgram {
+    class GameView {
+        public static void viewBoard(Board board) {
+            for(int i = 0; i < board.rows; i++) {
+                for (int j = 0; j < board.columns; j++) {
+                    if (board.piece(i, j) == null)
+                        Console.Write("-");
+                    Console.Write(board.piece(i, j) + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
--- ChessProgram/board/Board.cs
+++ ChessProgram/board/Board.cs
@@ -9,5 +9,9 @@
             this.columns = columns;
             pieces = new Piece[rows, columns];
         }
+
+        public Piece piece(int row, int column) {
+            return pieces[row, column];
+        }
     }
 }
